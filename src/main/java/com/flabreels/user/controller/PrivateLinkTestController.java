d6e9d5f5b59486fc18f5a3e91b580b873a529800
Jongchan Ko <godol811@naver.com>
2023-01-05 20:23:12 +0900

    feature(Private link 기능 테스트):
    PrivateLinkTestController.java Added

--- src/main/java/com/flabreels/user/controller/PrivateLinkTestController.java
+++ src/main/java/com/flabreels/user/controller/PrivateLinkTestController.java
@@ -1,16 +1,32 @@
 package com.flabreels.user.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class PrivateLinkTestController {
 
-    @GetMapping("/link")
-    public String privateLinkTest(){
-        return "http://vpce-0880a5a57c91cd4cb-ttcv9iny.ecs.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?thatWorks?";
+    @GetMapping("/link1")
+    public ResponseEntity<String> privateLinkTest1(){
+        String baseUrl = "http://vpce-0880a5a57c91cd4cb-ttcv9iny.ecs.ap-northeast-2.vpce.amazonaws.com";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity(baseUrl,String.class);
+    }
+    @GetMapping("/link2")
+    public ResponseEntity<String> privateLinkTest2(){
+        String baseUrl = "vpce-0880a5a57c91cd4cb-ttcv9iny-ap-northeast-2b.ecs.ap-northeast-2.vpce.amazonaws.com";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity(baseUrl,String.class);
+    }
+    @GetMapping("/link3")
+    public ResponseEntity<String> privateLinkTest3(){
+        String baseUrl = "http://vpce-0880a5a57c91cd4cb-ttcv9iny-ap-northeast-2a.ecs.ap-northeast-2.vpce.amazonaws.com";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity(baseUrl,String.class);
     }
 
 }
