eee756345d6c937bfc4b8863c22bccb8fe33b439
Jongchan Ko <godol811@naver.com>
2023-01-07 16:12:52 +0900

    feature(Private link 기능 테스트):
    PrivateLinkTestController.java Added

--- src/main/java/com/flabreels/user/controller/PrivateLinkTestController.java
+++ src/main/java/com/flabreels/user/controller/PrivateLinkTestController.java
@@ -12,21 +12,26 @@ public class PrivateLinkTestController {
 
     @GetMapping("/link1")
     public ResponseEntity<String> privateLinkTest1(){
-        String baseUrl = "http://vpce-0b197abb4703b66bb-0k126xpg.vpce-svc-0563e2b3d8fc797cf.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
+        String baseUrl = "http://vpce-06eeb43bad61fdb7f-ie0pn8k6.vpce-svc-00bfa833a343b6727.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForEntity(baseUrl,String.class);
     }
     @GetMapping("/link2")
     public ResponseEntity<String> privateLinkTest2(){
-        String baseUrl = "http://vpce-0b197abb4703b66bb-0k126xpg-ap-northeast-2a.vpce-svc-0563e2b3d8fc797cf.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
+        String baseUrl = "http://vpce-06eeb43bad61fdb7f-ie0pn8k6-ap-northeast-2a.vpce-svc-00bfa833a343b6727.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForEntity(baseUrl,String.class);
     }
     @GetMapping("/link3")
     public ResponseEntity<String> privateLinkTest3(){
-        String baseUrl = "http://vpce-0b197abb4703b66bb-0k126xpg-ap-northeast-2b.vpce-svc-0563e2b3d8fc797cf.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
+        String baseUrl = "http://vpce-06eeb43bad61fdb7f-ie0pn8k6-ap-northeast-2b.vpce-svc-00bfa833a343b6727.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForEntity(baseUrl,String.class);
     }
-
+    @GetMapping("/link4")
+    public ResponseEntity<String> privateLinkTest4(){
+        String baseUrl = "http://naver.com";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity(baseUrl,String.class);
+    }
 }
