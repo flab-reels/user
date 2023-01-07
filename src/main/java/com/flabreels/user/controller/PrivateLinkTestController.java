d831015d85da31721974b0f23e53898a0819275b
Jongchan Ko <godol811@naver.com>
2023-01-07 17:09:32 +0900

    feature(Private link 기능 테스트):
    PrivateLinkTestController.java Added

--- src/main/java/com/flabreels/user/controller/PrivateLinkTestController.java
+++ src/main/java/com/flabreels/user/controller/PrivateLinkTestController.java
@@ -12,19 +12,19 @@ public class PrivateLinkTestController {
 
     @GetMapping("/link1")
     public ResponseEntity<String> privateLinkTest1(){
-        String baseUrl = "vpce-0e4bd3120367e8953-ykop0aoh.vpce-svc-0563e2b3d8fc797cf.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
+        String baseUrl = "vpce-01cb36638e6ec5736-uwdwv5q0.vpce-svc-01343629200457007.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForEntity(baseUrl,String.class);
     }
     @GetMapping("/link2")
     public ResponseEntity<String> privateLinkTest2(){
-        String baseUrl = "vpce-0e4bd3120367e8953-ykop0aoh-ap-northeast-2a.vpce-svc-0563e2b3d8fc797cf.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
+        String baseUrl = "vpce-01cb36638e6ec5736-uwdwv5q0-ap-northeast-2b.vpce-svc-01343629200457007.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForEntity(baseUrl,String.class);
     }
     @GetMapping("/link3")
     public ResponseEntity<String> privateLinkTest3(){
-        String baseUrl = "vpce-0e4bd3120367e8953-ykop0aoh-ap-northeast-2b.vpce-svc-0563e2b3d8fc797cf.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
+        String baseUrl = "vpce-01cb36638e6ec5736-uwdwv5q0-ap-northeast-2a.vpce-svc-01343629200457007.ap-northeast-2.vpce.amazonaws.com/privatelink/privatelink?godol";
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForEntity(baseUrl,String.class);
     }
